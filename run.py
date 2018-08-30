baf731ff8792cbc1222c34e469f4da0d421eda04
PikalaxALT <pikalaxalt@gmail.com>
2018-08-30 17:02:31 -0400

    Upgrade for rewrite compliance

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Panopticon by Megumi Sonoda
 Copyright 2016, Megumi Sonoda
 This file is licensed under the BSD 3-clause License
-'''
+Upgraded to support discord.py 1.0.0a1540 by PikalaxALT
+"""
 
 # Imports from stdlib
 import asyncio
 import base64
-from datetime import datetime
-from datetime import timezone
+from datetime import datetime, timezone
+import time
 import os
 import re
 import signal
@@ -18,11 +19,11 @@ import sys
 
 # Imports from dependencies
 import discord
-from discord.enums import ChannelType
+from discord.ext import commands
 
 # Import configuration
 from config import (
-    TOKEN, BOT_ACCOUNT,
+    TOKEN, COMMAND_PREFIX,
     USE_LOCALTIME, LOG_DIR,
     MAX_MESSAGES, AWAY_STATUS
 )
@@ -51,29 +52,29 @@ def clean_filename(string):
 # This builds the relative file path & filename to log to,
 #   based on the channel type of the message.
 # It is affixed to the log directory set in config.py
-def make_filename(message):
-    if message.edited_timestamp:
-        time = message.edited_timestamp
+def make_filename(message: discord.Message):
+    if message.edited_at:
+        time = message.edited_at
     else:
-        time = message.timestamp
+        time = message.created_at
     timestamp = time.strftime('%F')
-    if message.channel.type == ChannelType.text:
+    if isinstance(message.channel, discord.TextChannel):
         return "{}/{}-{}/#{}-{}/{}.log".format(
             LOG_DIR,
-            clean_filename(message.server.name),
-            message.server.id,
+            clean_filename(message.guild.name),
+            message.guild.id,
             clean_filename(message.channel.name),
             message.channel.id,
             timestamp
         )
-    elif message.channel.type == ChannelType.private:
+    elif isinstance(message.channel, discord.DMChannel):
         return "{}/DM/{}-{}/{}.log".format(
             LOG_DIR,
-            clean_filename(message.channel.user.name),
-            message.channel.user.id,
+            clean_filename(message.author.name),
+            message.author.id,
             timestamp
         )
-    elif message.channel.type == ChannelType.group:
+    elif isinstance(message.channel, discord.GroupChannel):
         return "{}/DM/{}-{}/{}.log".format(
             LOG_DIR,
             clean_filename(message.channel.name),
@@ -88,11 +89,11 @@ def make_filename(message):
 # Message ID will be base64-encoded since it becomes shorter that way.
 # If the message was edited, prefix messageid with E:
 #   and use the edited timestamp and not the original.
-def make_message(message):
+def make_message(message: discord.Message):
     # Wrap the message ID in brackets, and prefix E: if the message was edited.
     # Also, base64-encode the message ID, because it's shorter.
     #   This uses less space on disk, and is easier to read in console.
-    message_id = '[E:' if message.edited_timestamp else '['
+    message_id = '[E:' if message.edited_at else '['
     message_id += "{}]".format(base64.b64encode(
         int(message.id).to_bytes(8, byteorder='little')
     ).decode('utf-8'))
@@ -100,10 +101,10 @@ def make_message(message):
     # Get the datetime from the message
     # If necessary, tell the naive datetime object it's in UTC
     #   and convert to localtime
-    if message.edited_timestamp:
-        time = message.edited_timestamp
+    if message.edited_at:
+        time = message.edited_at
     else:
-        time = message.timestamp
+        time = message.created_at
     if USE_LOCALTIME:
         time = time.replace(tzinfo=timezone.utc).astimezone(tz=None)
 
@@ -128,7 +129,7 @@ def make_message(message):
     attachments = ''
     if message.attachments:
         for attach in message.attachments:
-            attachments += '\n(attach) {0[url]}'.format(attach)
+            attachments += '\n(attach) {0.url}'.format(attach)
 
     # Use all of this to return as one string
     return("{} {} {} {} {}".format(
@@ -143,50 +144,65 @@ def make_message(message):
 # Append to file, creating path if necessary
 def write(filename, string):
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+    exists = os.path.exists(filename)
     with open(filename, 'a', encoding='utf8') as file:
+        if not exists:
+            dt_object = datetime.now() if USE_LOCALTIME else datetime.utcnow()
+            starthour = 11 if time.daylight else 10
+            file.write('Session start: {} {}:00\n'.format(dt_object.strftime('%Y-%m-%D'), starthour))
         file.write(string + "\n") 
         #print(string, file=file)
 
 
-# Create client object
-client = discord.Client()
+# Create bot object
+bot = commands.Bot(COMMAND_PREFIX)
 
 
-# Register event handlers
-# On message send
-@client.event
-async def on_message(message):
-    if message.server and message.server.id in IGNORE_SERVERS:
+# Message action
+def handle_message(message: discord.Message):
+    if message.guild and message.guild.id in IGNORE_SERVERS:
         return
     filename = make_filename(message)
     string = make_message(message)
     write(filename, string)
+
+
+# Register event handlers
+# On message send
+@bot.listen()
+async def on_message(message: discord.Message):
+    handle_message(message)
 
 
 # On message edit
 # Note from discord.py documentation:
 #   If the message is not found in the Client.messages cache, then these
 #   events will not be called. This happens if the message is too old
-#   or the client is participating in high traffic servers.
-# Through testing, messages from before the current client session also do
+#   or the bot is participating in high traffic guilds.
+# Through testing, messages from before the current bot session also do
 #   not fire the event.
-@client.event
-async def on_message_edit(_, message):
-    if message.server and message.server.id in IGNORE_SERVERS:
+@bot.listen()
+async def on_message_edit(before: discord.Message, after: discord.Message):
+    handle_message(after)
+
+
+@bot.listen()
+async def on_raw_message_edit(payload: discord.RawMessageUpdateEvent):
+    channel = bot.get_channel(payload.data['channel_id'])
+    if channel is None or (channel.guild is not None and channel.guild.id in IGNORE_SERVERS):
         return
-    filename = make_filename(message)
-    string = make_message(message)
-    write(filename, string)
+    message = await channel.get_message(payload.data['id'])
+    handle_message(message)
 
 
 # On ready
-# Typically, a bot, self-bot or otherwise, has an always-green/'active'
+# Typically, a bot has an always-green/'active'
 #   status indicator. This provides the option to change the status when the
 #   actual user goes offline or away.
-@client.event
+@bot.listen()
 async def on_ready():
-    await client.change_presence(status=AWAY_STATUS)
+    await bot.change_presence(status=AWAY_STATUS)
 
 
-# Run client
-client.run(TOKEN, bot=BOT_ACCOUNT, max_messages=MAX_MESSAGES)
+# Run bot
+bot.run(TOKEN, max_messages=MAX_MESSAGES)
